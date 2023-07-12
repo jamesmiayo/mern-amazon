06e43f05fc52b089b1de404ea588477c410cd742
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-07-12 09:04:24 +0800

    edit homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -35,7 +35,9 @@ function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('/api/products');
+        const result = await axios.get(
+          'https://amazona-by-basir.onrender.com/api/products'
+        );
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
